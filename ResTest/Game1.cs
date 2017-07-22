b8a7425b6c4e803032cc678322d11b40bfd38c96
Ricardo Guevara Aragón <RickAr2@hotmail.com>
2017-07-22 11:10:13 -0600

    Agregado un proyecto FUNCIONAL y finalizado de independencia de resolucion basica. Creado un proyecto para prueba de gravedad, todavia no empezado.

---
+++ ResTest/Game1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ResTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D dropOfRedBlood;
+        Vector2 posicionRelativa;
+        bool estaLaKPresionada = false;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.ApplyChanges();
+            Global.Resex.Inicializar(this);
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            dropOfRedBlood = Content.Load<Texture2D>("DropOfRedBlood");
+            posicionRelativa = new Vector2(1220, 700);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if(Keyboard.GetState().IsKeyDown(Keys.K) && !estaLaKPresionada)
+            {
+                if (graphics.IsFullScreen)
+                    graphics.IsFullScreen = false;
+                else
+                    graphics.IsFullScreen = true;
+                graphics.ApplyChanges();
+                Global.Resex.Inicializar(this);
+                estaLaKPresionada = true;
+            }
+            else
+            {
+                estaLaKPresionada = false;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            Vector2 coordenadas = Global.Resex.TraducirCoordenadas(posicionRelativa);
+            Vector2 tam = Global.Resex.TraducirCoordenadas(new Vector2(10, 10));
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend, SamplerState.PointClamp);
+            spriteBatch.Draw(dropOfRedBlood, new Rectangle((int)coordenadas.X, (int)coordenadas.Y, (int)tam.X, (int)tam.Y), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ResTest/Global/Resex.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ResTest.Global
+{
+    public static class Resex
+    {
+        private static int PantallaAncho;
+        private static int PantallaAlto;
+
+        private static float VirtualAncho;
+        private static float VirtualAlto;
+
+        private static float ratioX;
+        private static float ratioY;
+
+        private static void CalcularRatio()
+        {
+            ratioY = PantallaAlto / VirtualAlto;
+            ratioX = PantallaAncho / VirtualAncho;
+        }
+
+        public static void Inicializar(Game1 game)
+        {
+            PantallaAlto = game.GraphicsDevice.Viewport.Height;
+            PantallaAncho = game.GraphicsDevice.Viewport.Width;
+
+            VirtualAlto = 720;
+            VirtualAncho = 1240;
+            CalcularRatio();
+        }
+
+        public static void CambiarResolucionVirtual(Game1 game, Vector2 nuevaRes)
+        {
+            Inicializar(game);
+            VirtualAlto = nuevaRes.Y;
+            VirtualAncho = nuevaRes.X;
+            CalcularRatio();
+        }
+
+        public static Vector2 TraducirCoordenadas(Vector2 coordenadas)
+        {
+            Vector2 nuevasCoordenadas = new Vector2(coordenadas.X * ratioX, coordenadas.Y * ratioY);
+            return nuevasCoordenadas;
+        }
+    }
+}
